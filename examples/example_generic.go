dca80517685e3c0d952417be7cf1372d56bbe408
Hulne <serge.hulne@gmail.com>
2022-03-23 11:10:13 +0100

    Add files via upload

--- examples/example_generic.go
+++ examples/example_generic.go
@@ -16,13 +16,13 @@ type Person struct {
 
 // - - -
 
-func input() Chan {
+func input() chan Person {
 
-	persons := []Generic{
-		Person{"Joe", 10},
-		Person{"Jane", 40},
-		Person{"Jim", 50},
-		Person{"John", 60},
+	persons := []Person{
+		{"Joe", 10},
+		{"Jane", 40},
+		{"Jim", 50},
+		{"John", 60},
 	}
 
 	persons_chan := Iterable_from_array(persons)
@@ -32,58 +32,42 @@ func input() Chan {
 
 // - - -
 
-type Gen struct {
-	value Generic
-}
-
-func (g *Gen) Next() Generic {
-	temp := g.value
-	g.value = temp.(int) + 1
-	return temp.(int)
-}
-
-func (g *Gen) HasNext() bool {
-	return g.value.(int) < 10
-}
-
-// - - -
-
-func main_generic() {
+func main() {
 
 	input_channel := input()
 
 	for item := range input_channel {
-		fmt.Printf("%v, %v\n", item.(Person).Name, item.(Person).Age)
+		fmt.Printf("%v, %v\n", item.Name, item.Age)
 	}
 
 	fmt.Println("- - -")
 	input_channel = input()
 
-	cb := func(c1, c2 Chan) Chan {
+	cb := func(c1, c2 chan Person) chan Person {
 		for person := range c1 {
-			if person.(Person).Age > 18 {
+			if person.Age > 18 {
 				p := Person{
-					strings.ToUpper(person.(Person).Name),
-					person.(Person).Age}
+					strings.ToUpper(person.Name),
+					person.Age}
 				c2 <- p
 			}
 		}
 		return c2
 	}
 
-	electors := Map(input_channel, cb)
+	electors := Filter(input_channel, cb)
 
 	for person := range electors {
-		fmt.Printf("Elector --> %v\n", person.(Person))
+		fmt.Printf("Elector --> %v\n", person)
 	}
 
-	nums := []Generic{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 	fmt.Println("- - -")
 	gen := Iterable_from_array(nums)
 
 	for item := range gen {
-		fmt.Printf("Gen: %#v\n", item.(int))
+		fmt.Printf("Gen: %#v\n", item)
 	}
 
 	fmt.Println("- - -")
@@ -91,7 +75,7 @@ func main_generic() {
 
 	every := Every(gen, 2)
 	for item := range every {
-		fmt.Printf("Every: %#v\n", item.(int))
+		fmt.Printf("Every: %#v\n", item)
 	}
 
 	fmt.Println("- - -")
@@ -99,7 +83,7 @@ func main_generic() {
 
 	skipped := Skip(gen, 2)
 	for item := range skipped {
-		fmt.Printf("Skipped: %#v\n", item.(int))
+		fmt.Printf("Skipped: %#v\n", item)
 	}
 
 	fmt.Println("- - -")
@@ -107,22 +91,9 @@ func main_generic() {
 
 	slice := Slice(gen, 4, 5)
 	for item := range slice {
-		fmt.Printf("Slice: %#v\n", item.(int))
+		fmt.Printf("Slice: %#v\n", item)
 	}
 
 	fmt.Println("- - -")
 
-	// Testing generator struct:
-	g := Gen{value: 0}
-	generated := Iterable_from_generator(&g)
-	for item := range generated {
-		fmt.Printf("generated: %v\n", item)
-	}
-
 }
-
-/*
-	TODO:
-		- From_file.
-		- From_url
-*/
