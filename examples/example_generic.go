d5b9e95eeb14dc9066671092d07e64bedc3a159d
Hulne <serge.hulne@gmail.com>
2022-06-30 22:07:14 +0200

    Update example_generic.go

--- examples/example_generic.go
+++ examples/example_generic.go
@@ -6,25 +6,35 @@ import (
 	. "github.com/serge-hulne/go_iter"
 )
 
+
+// Implements the interface generator
 type Counter struct {
-	Value   int
-	HasNext bool
+	value int
 }
 
 func (c *Counter) Next() {
-	if c.Value < 10 {
-		c.Value++
-		c.HasNext = true
-	} else {
-		c.HasNext = false
+	if c.HasNext() {
+		c.value += 1
 	}
+}
+
+func (c *Counter) HasNext() bool {
+	if c.Value() < 10 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *Counter) Value() int {
+	return c.value
 }
 
 func main() {
 
-	c := Counter{0, true}
+	c := Counter{0}
 
-	it := Generator_to_Iterator(c)
+	it := Generator_to_Iterator[int](&c)
 
 	for v := range it {
 		fmt.Printf("%d ", v)
@@ -32,9 +42,9 @@ func main() {
 
 	// - - -
 
-	it = Generator_to_Iterator(c)
+	it = Generator_to_Iterator[int](&c)
 
-	it1 := Map(it, func(x int) int {
+	it1 := Map[int](it, func(x int) int {
 		return 2 * x
 	})
 
@@ -48,7 +58,7 @@ func main() {
 
 	println("\n- - -")
 
-	it = Generator_to_Iterator(c)
+	it = Generator_to_Iterator[int](&c)
 
 	it1 = Filter(it, func(x int) bool {
 		return x%2 == 0
@@ -62,7 +72,8 @@ func main() {
 
 	println("\n- - -")
 
-	it = Generator_to_Iterator(c)
+	it = Generator_to_Iterator[int](&c)
+
 	it1 = Filter(it, func(x int) bool {
 		return x%2 == 0
 	})
@@ -75,7 +86,8 @@ func main() {
 
 	println("\n- - -")
 
-	it = Generator_to_Iterator(c)
+	it = Generator_to_Iterator[int](&c)
+
 	every := Every(it, 3)
 
 	for v := range every {
@@ -83,8 +95,7 @@ func main() {
 	}
 
 	println("\n- - -")
-
-	it = Generator_to_Iterator(c)
+	it = Generator_to_Iterator[int](&c)
 
 	skipped := Skip(it, 4)
 
@@ -94,12 +105,45 @@ func main() {
 
 	println("\n- - -")
 
-	it = Generator_to_Iterator(c)
+	it = Generator_to_Iterator[int](&c)
 
 	top := Take(it, 5)
 
 	for v := range top {
 		fmt.Printf("%d ", v)
+	}
+
+	// - - -
+
+	println("\n- - -")
+
+	// Data
+	nums := []int{0, 1, 2, 3, 4, 5}
+
+	// Data -> iterator:
+	generated := Iterable_from_Array(nums)
+
+	// -> 0, 1, 2, 3, 4, 5
+
+	// Mapping x -> 2 * x for all elements of the iterator:
+	even := Map(generated, func(x int) int {
+		return 2 * x
+	})
+
+	// -> 0, 2, 4, 6, 8, 10
+
+	// Mapping all the elements of 'even' to it's square value:
+	even_and_squared := Map(even, func(x int) int {
+		return x * x
+	})
+
+	// -> 0, 4, 16, 36, 64, 100
+
+	println("Final")
+
+	// Displaying results:
+	for item := range even_and_squared {
+		fmt.Printf("%#v ", item)
 	}
 
 }
